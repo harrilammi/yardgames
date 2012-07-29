ed4ac62918d25422a299d9a8ddf01b8d0eeaec78
Harri Lammi <harri.lammi@leonidasoy.fi>
2012-07-29 14:11:46 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yardgames::Application.config.secret_token = 'b868bcde1fcfecfb481253ff05a17dd887381b75afef4d898bf7a8f27aeb81ce2fbd3076a46c794ad3bece8809de3f596d09a2d51634ff5d9f2ceceb55255c47'
