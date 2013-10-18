33375afc2da72ce4d227f1b41d58f86c856fc7e2
Harri Lammi <harri.lammi@gmail.com>
2013-10-18 21:31:39 +0300

    game stuff

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,22 @@
 class GamesController < ApplicationController
+
+  def index
+    @game = Game.new
+    @teams = Team.all
+  end
+
+  def create
+    @game = Game.new(params[:game])
+    if @game.save
+      flash[:notice] = "Peli tallennettu."
+    else
+      @teams = Team.all
+      render :index
+    end
+    redirect_to game_path(@game)
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :home_team_id, :visitor_team_id
+
+  belongs_to :home_team, class_name: "Team", foreign_key: "home_team_id"
+  belongs_to :visitor_team, class_name: "Team", foreign_key: "visitor_team_id"
 end
